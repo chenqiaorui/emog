d360ed0ebe5cee4f073e34547a395966181c692a
chenqiaorui <chenqiaorui@staff.easub.com>
2022-11-03 18:24:10 +0800

    added valid and 白名单

--- src/main/java/com/emog/controller/UmsAdminController.java
+++ src/main/java/com/emog/controller/UmsAdminController.java
@@ -4,11 +4,11 @@ import com.emog.common.CommonResult;
 import com.emog.dto.UmsAdminParam;
 import com.emog.model.UmsAdmin;
 import com.emog.service.UmsAdminService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin")
@@ -17,7 +17,11 @@ public class UmsAdminController {
     @Autowired
     UmsAdminService adminService;
 
-    public CommonResult<UmsAdmin> register(@Validated @RequestParam UmsAdminParam adminParam) {
+    @ApiOperation(value = "用户注册")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult<UmsAdmin> register(@Validated @RequestBody UmsAdminParam adminParam) {
+        System.out.println("UmsAdmin call...");
         UmsAdmin admin = adminService.register(adminParam);
         if(admin != null) {
             return CommonResult.success(admin);
