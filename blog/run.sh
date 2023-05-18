92b55266ace8ac16e390380694379eb8d76885bf
chenqiaorui <chenqiaorui@staff.easub.com>
2023-05-18 18:19:05 +0800

    更新主题

--- blog/run.sh
+++ blog/run.sh
@@ -1,16 +1,19 @@
-push_code() {
-    git add . && \
-    git commit -m"update" \
-    git push 
-}
-depoly() {
-    hexo clean && \ 
+# push_code() {
+#     git add . && \
+#     git commit -m"update" \
+#     git push 
+# }
+# depoly() {
+#     hexo clean && \ 
+#     hexo g && \
+#     hexo d
+# }
+
+# if [ $1 == 'push' ]
+#     push_code
+# fi
+
+# deploy
+hexo clean && \ 
     hexo g && \
     hexo d
-}
-
-if [ $1 == 'push' ]
-    push_code
-fi
-
-deploy
